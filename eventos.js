ec6ea077427b90db18091948371347c7968ca103
Daniel-Abreu-94 <141192601+Daniel-Abreu-94@users.noreply.github.com>
2023-08-08 20:07:02 -0300

    fix button event

--- eventos.js
+++ eventos.js
@@ -1,4 +1,5 @@
 const divClickArea = document.getElementById("buttonContainer");
-divClickArea.addEventListener("click", function() {
+divClickArea.addEventListener("click", function(event) {
     alert("Hola! Soy el div");
+    event.stopPropagation();
 });
